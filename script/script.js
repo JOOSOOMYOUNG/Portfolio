9c6dc822b9f757dd49072574ff8c2def87053d4e
JOOSOOMYEONG <JOO2JOJ@GMAIL.COM>
2025-06-23 01:12:42 +0900

    -

---
+++ script/script.js
@@ -0,0 +1,11 @@
+// 헤더 상단 현재 시간
+const time = document.getElementById("time");
+function getTime() {
+  const date = new Date();
+  const hour = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const second = String(date.getSeconds()).padStart(2, "0");
+  time.innerText = `${hour} : ${minutes} : ${second}`;
+}
+getTime();
+setInterval(getTime, 1000);
